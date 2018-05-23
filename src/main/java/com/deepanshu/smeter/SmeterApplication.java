9ea7f039084a22432da66cd8ccee4df7f40fe902
deepanshut041 <deepanshut041@gmail.com>
2018-05-23 22:08:56 +0530

    Inital commit, initialize project from spring starter

---
+++ src/main/java/com/deepanshu/smeter/SmeterApplication.java
@@ -0,0 +1,12 @@
+package com.deepanshu.smeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmeterApplication.class, args);
+	}
+}
